9434949f12e2c6a790aa6dc46d38219961d8dec6
fazlyedgenroots <fazly@edgenroots.com>
2024-06-18 01:06:05 +0600

    db connection stablished

--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -4,16 +4,11 @@ import mongoose from "mongoose";
 let isConnected = false;
 let connection = null;
 
-console.log(isConnected, "isConnected");
-console.log(connection, "connection");
 
 const dbConnect = async () => {
-    console.log("inside db connect");
-    console.log(process.env.MONGODB_URI, "MONGODB_URI");
     try {
         if (isConnected) return connection
         connection = await mongoose.connect(String(process.env.MONGODB_URI));
-        console.log(connection, "connection");
         isConnected = true;
         return connection
     } catch (e) {
